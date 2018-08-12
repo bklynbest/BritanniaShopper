3bdf69553c4e7e9bd43900cfe4bbdf3570661d41
Jonathan Momperousse <bklynbest718@gmail.com>
2018-08-12 20:36:04 +0000

    added category model and set relation with product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ActiveRecord::Base
-    has_many :orders 
+    has_many :orders
+    mount_uploader :image, ImageUploader
+    belongs_to :categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180812025113) do
+ActiveRecord::Schema.define(version: 20180812203421) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
@@ -23,10 +29,13 @@ ActiveRecord::Schema.define(version: 20180812025113) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "name"
     t.integer  "price"
+    t.string   "image"
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|

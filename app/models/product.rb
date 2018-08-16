cec7bb09585422f1239ee7ce0e5d7e071b22a44c
Jonathan Momperousse <bklynbest718@gmail.com>
2018-08-16 07:05:48 +0000

    updated landing page

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ActiveRecord::Base
     has_many :orders
     belongs_to :category
     validates :name, :price, presence: true
+    validates :category, presence: true
     mount_uploader :image, ImageUploader
 end
